b7a9042ffadc089a94f081b195501cc00c8d7923
Steve B <evetston@icloud.com>
2025-12-13 18:32:31 -0600

    Added comments and hopefully improved readability.

--- popo.py
+++ popo.py
@@ -2,15 +2,26 @@ import board
 import neopixel
 import time
 
+# The LED string is 10 LEDs, so consider it split into a left and right half.
+# We will alternately flash different colors on left and right side. The red
+# blue and white colors are defined in the list 'c'. The 'rgw' list has the
+# pairings of left/right colors for each flashing iteration.
+
+r=0
+b=1
+w=2
+c = ((255, 0, 0), (0,0,255), (127,127,127))
+rgw = ((r,b),(r,w),(b,r),(w,b),(b,r))
+
 pixels = neopixel.NeoPixel(board.D18, 10, auto_write=False)
 
-rgw = ((0,1),(0,2),(1,0),(2,1),(1,0))
-c = ((255, 0, 0), (0,0,255), (127,127,127))
 
 # Flash a few LEDs white to give the look of police lights.
 
 for r in range(5):
+    # Loop over our color pairs, doing two kinds of alternating flashing.
     for n in range(5):
+        # First, flash three times quickly per left/right side.
         for f in range(3):
             lc = c[rgw[n][0]]
             for i in range(5):
@@ -31,6 +42,7 @@ for r in range(5):
             pixels.show()
             time.sleep(0.05)
 
+        # Now simply flash a bit more slowly back and forth.
         for f in range(3):
             lc = c[rgw[n][0]]
             for i in range(5):
